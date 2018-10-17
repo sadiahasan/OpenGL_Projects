f85aa030884a76b22bf5852d88c940fc02e62eb4
Sadia Hasan <sadiazh729@gmail.com>
2018-10-17 13:55:12 -0400

    initial commit

---
+++ concave_convex_circles/concave_convex_circles.cpp
@@ -0,0 +1,117 @@
+////  Sadia Hasan
+////  CS368
+////  Project 1
+
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <array>
+
+/* makeCircle method
+ * makes circle and puts in random spot on screen
+ */
+
+void makeCircle(float radius, float rX, float rY){
+    double w = 0.0;
+    double b = 1.0;
+    bool con = rand() & 1; //random bool value to determine if concave or convex
+    
+    for(int i = 0; i < 100; i++){ //for loop to make circle out of lines
+        float y = -radius + (i*2*radius) /100;
+        float x = sqrt(radius*radius -(y*y));
+        
+        w=w+.01;
+        b = 1.0 - w;
+        
+        if(con) { // convex
+            glColor3f(w, w, w);
+        }else{ //convex
+            glColor3f(b, b, b);
+        }
+        glVertex2f(-x + rX, y -rY);
+        glVertex2f(x + rX, y -rY);
+    }
+}
+
+/*
+ * noOverlap method
+ * checks viability of each potentiol circle by checking that its center point is not within the diameter of any other circles already created
+ */
+
+bool noOverlap(float z, float x, float y, float r, float a[]){
+    
+    double distance;
+    double diameter = 2*r+.2 * 10000;
+    
+    for(int j = 0; j < z*2; j+=2){
+        double x1 = a[j];
+        double y1 = a[j+1];
+        distance = sqrt(((x-x1)*(x-x1)) + ((y-y1)*(y-y1))) * 10000;
+        
+        if((diameter >= distance) ){
+            return 0;
+        }
+ }
+    return 1;
+ }
+
+void display(void){
+    
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_LINES);
+    int t = 0;
+    srand((unsigned)time(0));
+    int randNum = rand() % (10 + 1 - 5) + 5; //random number between 5 - 10, determines how many circles are created
+    
+    float points[randNum*2]; //array to hold x,y coordinates of center point of each circle
+    
+    for(int i = 0; i < randNum*2; i++){
+        points[i] = 0.0;
+    }
+    
+    float radius = .122; //radius of the circles
+    
+    for(int i = 0; i < randNum; i++){ //for loop iterates for each circle for randNum
+        
+        float randX = rand()%9999; //random value for x
+        randX = randX / 10000;
+        bool randboolX = rand() & 1; //bool determines if x is positive or negative
+            if(randboolX) randX = randX * -1;
+        
+        float randY = rand()%9999; //random value for y
+        randY = randY / 10000;
+        bool randboolY = rand() & 1; //bool determines if y is positive or negative
+            if(randboolY) randY = randY *-1;
+        
+        if(noOverlap(randNum,randX, randY, radius, points)){ //if circle has no overlap add to array
+            points[t++] = randX;
+            points[t++] = randY;
+            makeCircle(radius, randX, randY); //put circle on screen
+        }
+        else { //if circle will overlap decrease counter by 1
+            i--;
+        }
+    }
+    glEnd();
+    glFlush();
+}
+
+int main(int argc, char** argv){
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(800, 500);
+    glutInitWindowPosition(200, 100);
+    glutCreateWindow("circles");
+    glClearColor(0.5, 0.5, 0.5, 0.1);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-2.0, 2.0, -1.5, 1.5);
+    glutDisplayFunc(display);
+    glutMainLoop();
+    
+    
+}
---
+++ peripheral_drift/peripheral_drift.cpp
@@ -0,0 +1,106 @@
+////
+////  SADIA HASAN
+////  CS368
+////  Project 2
+////
+
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#include <array>
+#include <iostream>
+
+GLfloat rectangle[4][3] = {{8,0,0},{8,30,0},{-8,30,0},{-8,0,0}};
+GLboolean color = true;
+
+void draw_rectangle(void){
+    int b = 1;
+    int w = 0;
+    
+    glBegin(GL_QUADS); //draw rectangles
+    for(int i=0; i<4; i++){
+       glVertex3fv(rectangle[i]);
+    }
+    glEnd();
+    
+    if(color){ //for alternating borders between rings
+         b = 0;
+         w = 1;
+    }
+    
+    glLineWidth(4.0);
+    
+    glBegin(GL_LINE_STRIP); //half of rectangle's border
+    glColor3d(b, b, b);
+    glVertex2d(0, 0);
+    glVertex2d(8, 0);
+    glVertex2d(8, 30);
+    glVertex2d(0, 30);
+    glEnd();
+    
+    glBegin(GL_LINE_STRIP); //half of rectangle's border
+    glColor3d(w, w, w);
+    glVertex2d(0, 30);
+    glVertex2d(-8, 30);
+    glVertex2d(-8, 0);
+    glVertex2d(0, 0);
+    
+    glEnd();
+    
+}
+
+void draw_ring(void) {
+    
+    for(int r = 0; r<360; r+=45){ //draw ring of rectangles
+        glColor3f(0.2f,0.28f,1.34f); //color for rectangle
+        glPushMatrix();
+        glRotated(r, 0, 0, 1); // rotate at 45 degree angle
+        glTranslated(0, 35, 0);
+        draw_rectangle();
+        glPopMatrix();
+    }
+}
+
+void display(void) {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    int n = 0;
+    int y = 150;
+    
+    for(int i = 0; i < 8; i++){
+     //   glColor3f(0.2f,0.28f,1.34f);
+        glPushMatrix();
+        color = !color; //to alternate borders between rings
+        
+        int x = -218 + (n * 150); //x position of rings
+        n++;
+        
+        glTranslated(x, y, 0);
+        draw_ring();
+        glPopMatrix();
+        
+        if(x >= 232){ //reset for second row of rings
+            n = 0;
+            y = -120;
+            color = !color;
+        }
+    }
+    glFlush();
+}
+
+int main(int argc, char** argv){
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(700, 400);
+    glutInitWindowPosition(200, 100);
+    glutCreateWindow("Peripheral Drift");
+    glClearColor(126.0f /180.0f, 177.0f /180.0f, 1.0f /180.0f, 0.0); // color for background
+    glEnable(GL_DEPTH_TEST);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-300, 300, -300, 300, 0, 100);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glutDisplayFunc(display);
+    glutMainLoop();
+}
